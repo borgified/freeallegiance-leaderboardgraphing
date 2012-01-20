eebbd25f36e4b4b106cb9e492a2ae3352e052d49
Fwiffo <borgified@gmail.com>
2012-01-19 17:07:10 -0800

    theres some bug with the output file.. prob some logical error, id 17799 should appear on the file but it doesnt gotta revisit this later

--- db_remove_dupes.pl
+++ db_remove_dupes.pl
@@ -8,6 +8,8 @@ my $DEBUG=1;
 
 my $my_cnf = '~/scripts/secret/my_cnf.cnf';
 
+open(my $fh, '>', 'output.txt') or die "cant open $!";
+
 my $dsn =
   "DBI:mysql:;" .
   "mysql_read_default_file=$my_cnf";
@@ -20,13 +22,14 @@ my $dbh = DBI->connect(
 ) or  die "DBI::errstr: $DBI::errstr";
 
 my $query="select * from stats where callsign = 'fwiffo' limit 50";
+#my $query="select * from stats limit 10";
 my $sth=$dbh->prepare($query);
 $sth->execute();
 
-my @categories = qw/mu sigma rank wins losses draws defects stack_rating cmd_mu cmd_sigma cmd_rank cmd_wins cmd_losses cmd_draws kills ejects drn_kills stn_kills stn_caps kills_per_ejects hrs_played kills_per_hr timestamp/;
+my @categories = qw/id place callsign mu sigma rank wins losses draws defects stack_rating cmd_mu cmd_sigma cmd_rank cmd_wins cmd_losses cmd_draws kills ejects drn_kills stn_kills stn_caps kills_per_ejects hrs_played kills_per_hr timestamp status lgp/;
 
 #note: &haschanged assumes that the last item is timestamp (pops off last item)
-
+#status and lgp should be ignored as well
 
 my %stats;
 
@@ -42,10 +45,14 @@ while(my @row = $sth->fetchrow_array()){
 		if(&haschanged(@row)){
 			$DEBUG && print ">>>>>> stats change detected\n";
 			#get prev entries from hash and output to db table
-			$DEBUG && print "TODO: dumping values from hash to db table...\n";
+			#$DEBUG && print "TODO: dumping values from hash to db table...\n";
+			#we're gonna dump into a file instead of db table
+			$DEBUG && print "dumping old values to file...\n";
+			&dumptofile(@row);
+
 			#update all new stats in hash
-			$DEBUG && print "updating stats in hash\n";
-			my $i=3;
+			$DEBUG && print "updating stats in hash with new values\n";
+			my $i=0;
 			foreach my $item (@categories){
 				$stats{$row[2]}{$item}=$row[$i];
 				$i++;
@@ -55,33 +62,39 @@ while(my @row = $sth->fetchrow_array()){
 			$DEBUG && print "duplicate confirmed\n";
 			#this is duplicate
 			#update timestamp
-			$stats{$row[2]}{'timestamp'}=$row[-1];
+			$stats{$row[2]}{'id'}=$row[0];
+			$stats{$row[2]}{'timestamp'}=$row[-3];
 		} 
 
 	}else{
 		#callsign doesnt exist
-		$DEBUG && print "adding new entry: $row[2]\n";
-		my $i=3;
+		$DEBUG && print "adding new entry to hash: $row[2]\n";
+		my $i=0;
 		foreach my $item (@categories){
 			$stats{$row[2]}{$item}=$row[$i];
 			#print "$row[2] : $item $row[$i]\n";
 			$i++;
 		}
+		$DEBUG && print "dumping to file\n";
+		$"=',';
+		print $fh "@row\n";
+		$"=' ';
 	}
 }
 
 sub haschanged {
-#returns 0 if all stats except timestamp have not changed
-#returns 1 if any stats except timestamp have changed
+#returns 0 if all stats except timestamp,status,lgp have not changed
+#returns 1 if any stats except timestamp,status,lgp have changed
 	my @row = @_;
-
-	my @categories_no_tstamp = @categories;
-	pop(@categories_no_tstamp); #remove the last category, timestamp
 
 	my $callsign=$row[2];
 
-	my $i=3;
-	foreach my $item (@categories_no_tstamp){
+	my $i=3; #ignoring first three fields (id,place,callsign)
+	foreach my $item (@categories){
+		if($item =~/id|place|callsign|timestamp|status|lgp/){
+		#skip the stuff that wouldnt make the stats different
+			next;
+		}
 		if($stats{$callsign}{$item} != $row[$i]){
 			$DEBUG && print "hash stored value ($item): $stats{$callsign}{$item} \t encountered value: $row[$i]\n";
 			return 1;
@@ -91,3 +104,12 @@ sub haschanged {
 return 0;
 }
 
+sub dumptofile {
+	my @row = @_;
+	my $output = "=";
+	foreach my $item (@categories){
+		$output = $output . "$stats{$row[2]}{$item},";
+	}
+	chop($output); #remove that last comma
+	print $fh "$output\n";
+}
